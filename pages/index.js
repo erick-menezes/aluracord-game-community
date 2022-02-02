a470fde56b463b78c43f0e7aa755a4a03e3bf636
erick-menezes <erickmenezes25@gmail.com>
2022-02-02 17:08:13 -0300

    Resultado final do evento

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,7 @@
 import { Box, Button, Text, TextField, Image, Icon } from '@skynexui/components'; 
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>
-            {`
-                * {
-                    margin: 0;
-                    padding: 0;
-                    box-sizing: border-box;
-                    list-style: none;
-                }
-
-                body {
-                    font-family: 'Open Sans', sans-serif;
-                }
-
-                html, body, #__next {
-                    min-height: 100vh;
-                    display: flex;
-                    flex: 1;
-                }
-
-                #__next {
-                    flex: 1;
-                }
-                
-                #__next > * {
-                    flex: 1;
-                }
-            `}
-        </style>
-    )
-}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -54,25 +22,17 @@ function Title(props) {
     );
 }
 
-// function HomePage() {
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Title>Boas vindas de volta!</Title>
-//             <Title tag="h2">Discord - Game center</Title>
-//         </div>
-
-//     )
-// }
-  
-// export default HomePage
-
 export default function PaginaInicial() {
     const [username, setUsername] = useState('');
-  
+    const router = useRouter();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        router.push(`/chat?username=${username}`);
+    }
+    
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -98,6 +58,7 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={(e) => handleSubmit(e)}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -153,7 +114,7 @@ export default function PaginaInicial() {
               }}
             >
                 {
-                    username ? (
+                    username.length > 2 ? (
                         <>
                             <Image
                                 styleSheet={{
